00d1dd9d2113b90612fd0be57a682350266e384d
muhyid <83450450+muhyid@users.noreply.github.com>
2022-08-23 14:31:27 +0300

    Add files via upload

---
+++ auto-install.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+set -e
+
+# Set your environment variables here
+JMETER_NAMESPACE=vrp-loadtesting
+JMETER_APP_NAME=jmeter
+JMETER_GIT_REPO=https://github.com/muhyid/jmeter.git
+JMETER_TEST="example"
+MEMORY_REQUEST="1024"
+MEMORY_LIMIT="2048"
+CPU_REQUEST="1.0"
+CPU_LIMIT="2.0"
+
+#############################
+## Do not modify anything from this line
+#############################
+
+# Print environment variables
+echo -e "\n=============="
+echo -e "ENVIRONMENT VARIABLES:"
+echo -e " * JMETER_NAMESPACE: $JMETER_NAMESPACE"
+echo -e " * JMETER_APP_NAME: $JMETER_APP_NAME"
+echo -e " * JMETER_GIT_REPO: $JMETER_GIT_REPO"
+echo -e "==============\n"
+
+# Check if the user is logged in
+if ! oc whoami &> /dev/null; then
+    echo -e "Check. You are not logged out. Please log in and run the script again."
+    exit 1
+else
+    echo -e "Check. You are correctly logged in. Continue..."
+fi
+
+#craete the ns if its not there
+# oc create ns $JMETER_NAMESPACE || true
+#oc project $JMETER_NAMESPACE # To avoid issues with deleted projects
+
+# Create JMeter configuration on ConfigMap
+echo -e "\n[1/3]Creating JMeter configuration on ConfigMap"
+oc delete configmap ${JMETER_APP_NAME}-config -n $JMETER_NAMESPACE || true
+oc create configmap ${JMETER_APP_NAME}-config -n $JMETER_NAMESPACE \
+--from-file=${JMETER_TEST}.jmx=tests/${JMETER_TEST}/test.jmx \
+--from-file=config.properties=tests/${JMETER_TEST}/config-k8s.properties
+oc get cm
+# Create RHDG Client configmap
+
+
+
+echo -e "\n[2/3]Building the JMeter container image"
+oc process -f templates/jmeter-bc.yaml \
+    -p APP_NAMESPACE=$JMETER_NAMESPACE \
+    -p APPLICATION_NAME=$JMETER_APP_NAME \
+    -p GIT_REPOSITORY=$JMETER_GIT_REPO | oc apply -f -
+
+# Deploy the RHDG client
+echo -e "\n[3/3]Deploying the JMeter client"
+oc process -f templates/jmeter-dc.yaml \
+    -p APP_NAMESPACE=$JMETER_NAMESPACE \
+    -p APPLICATION_NAME=$JMETER_APP_NAME \
+    -p MEMORY_REQUEST=$MEMORY_REQUEST \
+    -p MEMORY_LIMIT=$MEMORY_LIMIT \
+    -p CPU_REQUEST=$CPU_REQUEST \
+    -p CPU_LIMIT=$CPU_LIMIT \
+    -p TEST_NAME=$JMETER_TEST | oc apply -f -
+
+sleep 5
+# Wait for DeploymentConfig
+echo -n -e "\nWaiting for pods ready..."
+while [[ $(oc get pods -l app=$JMETER_APP_NAME -n $JMETER_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
+
+JMETER_POD=$(oc get pods -l app=$JMETER_APP_NAME -n $JMETER_NAMESPACE --template='{{(index .items 0).metadata.name}}')
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
+mkdir -p ./results/ocp-$NOW-$JMETER_TEST-report
+
+echo -e "\JMeter pod information:"
+echo -e " * POD: $JMETER_POD"
+echo -e " * LOGS: oc logs -f $JMETER_POD"
+echo -e " * REPORT: oc rsync $JMETER_POD:/opt/jmeter/results/$JMETER_TEST.zip ./results/ocp-$NOW-$JMETER_TEST-report"
---
+++ container-image/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Inspired from https://github.com/justb4/docker-jmeter/blob/master/entrypoint.sh
+# Basically runs jmeter, assuming the PATH is set to point to JMeter bin-dir (see Dockerfile)
+#
+# This script expects the standard JMeter command parameters.
+#
+set -e
+# Initial Java heap size for the Eden generation
+# xmn=$(($MEMORY_LIMIT/1024/1024))
+# Thread size
+#xss="2048"
+# Maximum heap size
+xmx=$(($MEMORY_LIMIT/1024/1024/2))
+# Initial Java heap size
+xms=$xmx
+
+# export JVM_ARGS="-Xmn${xmn}m -Xms${xms}m -Xmx${xmx}m"
+export JVM_ARGS="-Xms${xms}m -Xmx${xmx}m"
+
+echo "JVM_ARGS=${JVM_ARGS}"
+echo "jmeter args=$@"
+
+# Ejecute the JMeter command.
+if [ $RUN_JMETER != false ]
+then
+    echo "START Running Jmeter on `date`"
+    # -n(--nongui),-D(--systemproperty),-t(--testfile),-l(--logfile)
+    # -p(--propfile),-e(--reportatendofloadtests),-o(--reportoutputfolder)
+    export TEST_PLAN="${TEST_NAME:-example}"
+    jmeter \
+    -n \
+    -q "$JMETER_BASE/tests/config.properties" \
+    -t "$JMETER_BASE/tests/${TEST_PLAN}.jmx" \
+    -l "$JMETER_BASE/results/${TEST_PLAN}.jtl" \
+    -e \
+    -o "$JMETER_BASE/results/${TEST_PLAN}-report"
+    exec tail -f jmeter.log
+    echo "END Running Jmeter on `date`"
+    echo "Zipping report"
+    zip -r $JMETER_BASE/results/${TEST_PLAN}.zip $JMETER_BASE/results/${TEST_PLAN}-report
+else
+    echo "Skipping the execution of jmeter, run it manually..."
+fi
+
+# Wait to download results from the test
+while true;
+do
+    sleep 30;
+done;
